436382207bba36567310f19c5f6201761819fe9d
MojieY <MojieY>
2014-11-19 18:28:56 -0500

    FW_GPU.cu
    FW_GPU.cu

--- 未命名文件夹/FW_GPU.cu
+++ 未命名文件夹/FW_GPU.cu
@@ -1,10 +1,9 @@
-
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <cuda.h>
 
-
-#define MAXV 800
+#define MAXV 5
 #define max 999
 
 
@@ -16,31 +15,34 @@ typedef struct
 
 
 
-void Floyd(MGraph g)
+__global__ void Floyd(MGraph g)
 {
-    printf("%d\n", 0);
+   // printf("%d\n", 0);
     int A[MAXV][MAXV];
-    printf("%d\n", 1);
+   // printf("%d\n", 1);
     int path[MAXV][MAXV];
-    printf("%d\n", 2);
-    int i,j,k;
+   // printf("%d\n", 2);
+    //int i,j,k;
     //printf("%d\n", 1);
     int n=g.n;
-    
-    for(i=0;i<n;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-                A[i][j]=g.edges[i][j];
-                path[i][j]=-1;
+
+    int i = threadIdx.x;
+    int j = threadIdx.y;
+    int k = threadIdx.z;
+
+        for(int u = 0; u<n; u++){
+            for(int v = 0; v<n; v++){
+              A[u][v]=g.edges[u][v];
+              path[u][v]=-1;
             }
         }
-        for(k=0;k<n;k++)
-        {
-            for(i=0;i<n;i++){
-                for(j=0;j<n;j++){
+
+       // for(k=0;k<n;k++)
+       // {
+           // for(i=0;i<n;i++){
+              //  for(j=0; j<n; j++){
                     if(A[i][j]>(A[i][k]+A[k][j]))
-                    {
+{
                         A[i][j]=A[i][k]+A[k][j];
                         path[i][j]=k;
                     }
@@ -48,13 +50,16 @@ void Floyd(MGraph g)
                         A[i][j] = 0;
                     }
 
-                }
-                
-            }
-            
-           
+         //       }
+
+          //  }
+            for(int i = 0; i<n; i++){
+                for(int j = 0; j<n; j++){
+                        g.edges[i][j]=A[i][j];
         }
-    /*
+}
+
+}   /*
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
             if(A[i][j] == 999)
@@ -71,70 +76,90 @@ void Floyd(MGraph g)
     printf("/////////////////////////////\n");
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
-            
+
             printf("%d ", path[i][j]);
         }
         printf("\n");
     }
 */
-}
-
+//}
 int main(){
     MGraph graph;
-    malloc(MAXV*MAXV*sizeof(int));
-    
+    //malloc(MAXV*MAXV*sizeof(int));
+
     FILE *fp;
-    
+
     printf("Begin reading the file...\n");
-    
-    fp = fopen("/Users/yousumijie/Desktop/graph.txt","r");
-    
+
+    fp = fopen("graph.txt","r");
+
     fscanf(fp, "%d", &graph.n);
-    
+
     for(int i = 0; i<graph.n; i++){
         for(int j = 0; j<graph.n; j++){
             graph.edges[i][j]= max;
         }
     }
-   
+
     for(int i = 0; i<graph.n; i++){
         for(int j = 0; j<graph.n; j++){
-            
+
             fscanf(fp, "%d ",&graph.edges[i][j]);
-            
+
         }
     }
 
-    
+
    /* for(int i = 0; i<5; i++){
             fscanf(fp, "%d %d %d", &nodestart, &nodeend, &nodeWeight);
             graph.edges[nodestart][nodeend] = nodeWeight;
     }
     */
-    
-    if(!fp)
+if(!fp)
         fclose(fp);
     printf("Read file complete.\n");
     printf("the number of node is %d.\n", graph.n);
-   
+
     for(int i = 0; i<graph.n; i++){
         for(int j = 0; j<graph.n; j++){
-            
+
                 printf("%d ", graph.edges[i][j]);
-            
+
         }
         printf("\n");
     }
 
     printf("end\n");
-    
+
+    MGraph d_graph;
+
+    cudaMalloc((void**)&d_graph, sizeof(int)*(MAXV*MAXV+1));
+    cudaMemcpy(&d_graph, &graph,
+                        sizeof(int)*(MAXV*MAXV+1), cudaMemcpyHostToDevice
+                        );
     double start, stop, lapse;
     start = clock();
-    Floyd(graph);
+    Floyd<<<1,1,0>>>(d_graph);
     stop = clock();
     lapse = stop - start;
-    printf("time: %fs\n", lapse/CLOCKS_PER_SEC);
-    
-    return 0;
-    
+    printf("time: %fs\n", lapse);
+
+    cudaMemcpy(&graph, &d_graph,
+                        sizeof(int)*(MAXV*MAXV+1), cudaMemcpyDeviceToHost);
+
+        for(int i=0;i<graph.n;i++){
+                for(int j=0;j<graph.n;j++){
+                        if(graph.edges[i][j] == 999)
+            {
+                printf("%s ", "max");
+            }
+            else
+            {
+                printf("%d ", graph.edges[i][j]);
+            }
+        }
+        printf("\n");
+    }
+return 0;
+
 }
